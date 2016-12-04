b183b8ceaedebdc4acc479b3962df7eff0475d9b
RomarioVice <RomarioBrio@gmail.com>
2016-12-05 01:00:08 +0300

    vb

--- blocks/menu.php
+++ blocks/menu.php
@@ -1,10 +1,10 @@
 <!-- Pushy Menu -->
 <nav class="pushy pushy-left">
     <ul>
-        <li class="pushy-link"><a class="faa-parent animated-hover" href="index.php"><i class="fa fa-home fa-2x faa-pulse faa-fast" aria-hidden="true"></i> Главная</a></li>
-        <li class="pushy-link"><a class="faa-parent animated-hover" href="routes.php?page=1"><i class="fa fa-random fa-2x faa-pulse faa-fast" aria-hidden="true"></i> Маршруты</a></li>
-        <li class="pushy-link"><a class="faa-parent animated-hover" href="news.php?page=1"><i class="fa fa-newspaper-o fa-2x faa-pulse faa-fast" aria-hidden="true"></i> Новости</a></li>
-        <li class="pushy-link"><a class="faa-parent animated-hover" href="form.php?type=orders"><i class="fa fa-file-text fa-2x faa-pulse faa-fast" aria-hidden="true"></i> Подать заявку</a></li>
+        <li class="pushy-link"><a class="faa-parent animated-hover" href="index.php"><i class="fa fa-home fa-2x faa-pulse faa-fast" aria-hidden="true"></i> <strong>Главная</strong></a></li>
+        <li class="pushy-link"><a class="faa-parent animated-hover" href="routes.php?page=1"><i class="fa fa-random fa-2x faa-pulse faa-fast" aria-hidden="true"></i> <strong>Маршруты</strong></a></li>
+        <li class="pushy-link"><a class="faa-parent animated-hover" href="news.php?page=1"><i class="fa fa-newspaper-o fa-2x faa-pulse faa-fast" aria-hidden="true"></i> <strong>Новости</strong></a></li>
+        <li class="pushy-link"><a class="faa-parent animated-hover" href="form.php?type=orders"><i class="fa fa-file-text fa-2x faa-pulse faa-fast" aria-hidden="true"></i> <strong>Подать заявку</strong></a></li>
         <li class="pushy-link">&nbsp;</li>
         <li class="pushy-link">&nbsp;</li>
         <li class="pushy-link">&nbsp;</li>
@@ -13,12 +13,12 @@
         <li class="pushy-link">&nbsp;</li>
         <li class="pushy-link">&nbsp;</li>
         <li class="pushy-link">&nbsp;</li>
-        <li class="pushy-link">
-            <a href="https://vk.com/perevozki_dnr_rossiya">
-                <img class ="spin" src="img/VK.png" alt="Мы в VK.com"> VK.com</a></li>
-        <li class="pushy-link">
-            <a href="https://ok.ru/group52624225927358">
-                <img class ="spin" src="img/ox.png" alt="Мы в OK.ru"> OK.ru</a></li>
+        <li class="pushy-link"><a class="faa-parent animated-hover" href="https://vk.com/perevozki_dnr_rossiya"><i class="fa fa-vk fa-2x faa-pulse faa-fast" aria-hidden="true"></i> <strong>VK.com</strong></a></li>
+        <li class="pushy-link"><a class="faa-parent animated-hover" href="https://vk.com/perevozki_dnr_rossiya"><i class="fa fa-odnoklassniki fa-2x faa-pulse faa-fast" aria-hidden="true"></i> <strong>OK.ru</strong></a></li>
+<!--                <img class ="spin" src="img/VK.png" alt="Мы в VK.com"> VK.com</a>-->
+<!--        <li class="pushy-link">-->
+<!--            <a href="https://ok.ru/group52624225927358">-->
+<!--                <img class ="spin" src="img/ox.png" alt="Мы в OK.ru"> OK.ru</a></li>-->
     </ul>
 </nav>
 <!-- Site Overlay -->
